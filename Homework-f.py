38ebaaddba01bd3222c236d2cbcb1188c6935dfd
KhusnutdinovRustem <167807173+KhusnutdinovRustem@users.noreply.github.com>
2024-05-09 19:25:07 +0300

    Add files via upload

---
+++ Homework-f.py
@@ -0,0 +1,6 @@
+def print_params(x, y):
+    print('Я нахожусь по координатам ', x, y)
+    print('Они находятся по координатам ', y, x)
+
+print_params(29, 71)
+print_params(17, 83) #принимающая функция
